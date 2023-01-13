b6b6899e25ae9f64acda62243b44ee9a68bc508f
spielmannkim <shinjongk@gmail.com>
2023-01-13 10:53:50 +0900

    크냐?

---
+++ 4101.py
@@ -0,0 +1,9 @@
+#크냐?
+while True:
+    a, b = map(int, input().split())
+    if a == 0 and b == 0:
+        break # a와b에 둘다 0이 입력되면 무한루프 중단
+    if a > b: # 만약 a가 b보다 크다면 Yes 출력
+        print("Yes")
+    else:
+        print("No")
